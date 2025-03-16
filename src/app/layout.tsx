8d8d7c53692d4e33d025a50e7d1605f96d926201
Plsr <christian@gigs.com>
2025-03-16 07:23:13 +0100

    Update font to nunito sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Nunito_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const mainFont = Nunito_Sans({
+  variable: "--font-main",
   subsets: ["latin"],
 });
 
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${mainFont.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
